7b101400f242204cd14d06ddf3e28e1378fc0ae2
Vikas Rao <vikasrao1@gmail.com>
2026-04-28 17:26:26 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const keyArray = Array(26).fill(0);
+        const lookup = new Map();
+        for (let str of strs) {
+            let key = [...keyArray];
+            for (let ch of str) {
+                const keyChar = ch.charCodeAt(0) - "a".charCodeAt(0);
+                key[keyChar]++;
+            }
+            key = key.join(",")
+            if (!lookup.has(key)) lookup.set(key, []);
+            lookup.get(key).push(str);
+        }
+        return Array.from(lookup.values());
+    }
+}
