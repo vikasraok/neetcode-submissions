2a953b5e9bc880f567109ce9ea9610937b610d33
Vikas Rao <vikasrao1@gmail.com>
2026-04-28 18:42:27 +0530

    Add: longest-consecutive-sequence - submission-12

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-12.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let hash = new Set(nums);
+        let longest = 0;
+        for (let num of hash) {
+            if (!hash.has(num - 1)) {
+                let l = 1;
+                while (hash.has(num + l)) l++;
+                longest = Math.max(longest, l);
+            }
+        }
+        return longest;
+    }
+}
