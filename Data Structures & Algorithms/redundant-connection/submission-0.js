a3a513c884b235a04e5c71528ecfbe6f2843a7f6
Vikas Rao <vikasrao1@gmail.com>
2026-05-07 19:53:28 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[][]} edges
+     * @return {number[]}
+     */
+    union = (n) => {
+        const parent = Array(n)
+            .fill(null)
+            .map((_, i) => i);
+        const find = (x) => {
+            if (parent[x] !== x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        };
+        return (a, b) => {
+            const pa = find(a),
+                pb = find(b);
+            if (pa === pb) return false;
+            parent[pa] = pb;
+            return true;
+        };
+    };
+    findRedundantConnection(edges) {
+        const union = this.union(edges.length);
+        for (let [x, y] of edges) {
+            if (!union(x, y)) return [x, y];
+        }
+    }
+}
