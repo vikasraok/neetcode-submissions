90d27aeb77de29b060b6f93e655083da19444334
Vikas Rao <vikasrao1@gmail.com>
2026-05-07 19:02:41 +0530

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    union = (n) => {
+        const parent = Array.from({ length: n }, (_, i) => i);
+        const find = (x) => {
+            if (parent[x] !== x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        };
+        const union = (x, y) => {
+            const px = find(x),
+                py = find(y);
+            if (px === py) return false;
+            else {
+                parent[px] = py;
+            }
+            return true;
+        };
+        return { union };
+    };
+    countComponents(n, edges) {
+        const uf = this.union(n);
+        for (let [x, y] of edges) {
+            if (uf.union(x, y)) n--;
+        }
+        return n;
+    }
+}
