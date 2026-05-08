b0aab133ffb180c076a9df514760a9b995eb5078
Vikas Rao <vikasrao1@gmail.com>
2026-05-08 10:46:49 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @param {number[]} newInterval
+     * @return {number[][]}
+     */
+    insert(intervals, newInterval) {
+        const result = [];
+        let i = 0;
+        const n = intervals.length;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.push(intervals[i++]);
+        }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.push(newInterval);
+        while (i < n) result.push(intervals[i++]);
+        return result;
+    }
+}
