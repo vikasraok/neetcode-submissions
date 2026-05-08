2af6e554496c1f1ac7e11694b9e06a6ab5a86f2c
Vikas Rao <vikasrao1@gmail.com>
2026-05-08 11:31:49 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.js
@@ -0,0 +1,23 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        intervals.sort((a, b) => a.start - b.start);
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1].end > intervals[i].start) return false;
+        }
+        return true;
+    }
+}
