d7a5e98f9d56134c9ca160e44543144e003a6898
Vikas Rao <vikasrao1@gmail.com>
2026-05-08 23:42:58 +0530

    Add: house-robber-ii - submission-7

---
+++ Data Structures & Algorithms/house-robber-ii/submission-7.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if (nums.length === 1) return nums[0];
+        if (nums.length === 2) return Math.max(nums[0], nums[1]);
+        let second = nums[0];
+        let first = Math.max(nums[0], nums[1]);
+        for (let i = 2; i < nums.length - 1; i++) {
+            const curr = Math.max(first, second + nums[i]);
+            second = first;
+            first = curr;
+        }
+        let s = nums[1];
+        let f = Math.max(nums[1], nums[2]);
+        for (let i = 3; i < nums.length; i++) {
+            const curr = Math.max(f, s + nums[i]);
+            s = f;
+            f = curr;
+        }
+        return Math.max(first, f);
+    }
+}
