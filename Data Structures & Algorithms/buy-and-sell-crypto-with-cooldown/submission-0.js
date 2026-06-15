c7de9e58c4ddfa60663069d8e0ce9fa73e02d60d
Vikas Rao <vikasrao1@gmail.com>
2026-06-15 18:01:42 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let hold = -prices[0];
+        let sold = -Infinity;
+        let cool = 0;
+        for (let price of prices) {
+            const prevHold = hold;
+            const prevSold = sold;
+            const prevCool = cool;
+            hold = Math.max(prevHold, prevCool - price);
+            sold = prevHold + price;
+            cool = Math.max(prevCool, prevSold);
+        }
+        return Math.max(sold, cool)
+    }
+}
